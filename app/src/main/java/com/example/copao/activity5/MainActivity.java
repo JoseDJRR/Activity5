41870c6c1a20c6cf5feff03273445c4877c69d76
JOSE DE JESUS ROSADO RODRIGUEZ <AL02706907@tecmilenio.mx>
2018-10-04 18:42:13 -0500

    first commit

---
+++ app/src/main/java/com/example/copao/activity5/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.copao.activity5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    //String hello = getResources().getString(R.string.hello_world);
+
+}
